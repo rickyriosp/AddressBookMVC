adcdff3f6353f3729096701da48ff5b1a551ccb2
Ricky Rios <riki.rios91@gmail.com>
2021-08-02 21:55:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AddressBookMVC
+ Address Book application written in C# using the MVC pattern.
---
+++ Services/Interfaces/IImageService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace AddressBookMVC.Services.Interfaces
+{
+    public interface IImageService
+    {
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+
+        public string ConvertByteArrayToFile(byte[] fileData, string extension);
+    }
+}
